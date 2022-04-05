a57e8ca2c42ab968fae4d24015695502db0dd4cb
Memory Clutter <memclutter@gmail.com>
2022-04-05 09:37:50 +0300

    Ui for login form

--- web/spa/src/components/LoginForm.vue
+++ web/spa/src/components/LoginForm.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {reactive} from "vue";
 import authLogin from '@/apis/endpoints/auth/login'
 
-const email = ref('')
-const password = ref('')
+const form = reactive({
+  email: '',
+  password: ''
+})
 
 function submit() {
-  const {data, isFinished} = authLogin({email: email.value, password: password.value})
+  const {data, isFinished} = authLogin(form)
   console.log(data)
 }
 </script>
 
 <template>
-  <form @submit.stop.prevent="submit">
-    <label for="email">Email</label>
-    <input type="email" id="email" v-model="email">
-    <label for="password">Password</label>
-    <input type="password" id="password" v-model="password">
-    <input type="submit" value="Login"/>
-    <pre>
-      Email: {{email}}
-      Password: {{password}}
-    </pre>
-  </form>
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="Email">
+      <el-input type="email" v-model="form.email"/>
+    </el-form-item>
+    <el-form-item label="Password">
+      <el-input type="password" v-model="form.password"/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submit">Login</el-button>
+    </el-form-item>
+  </el-form>
 </template>
