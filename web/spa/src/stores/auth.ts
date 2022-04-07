65b0bb64abbc4c1541c140289261743648b076d0
Memory Clutter <memclutter@gmail.com>
2022-04-07 11:09:18 +0300

    Create auth pinia store

---
+++ web/spa/src/stores/auth.ts
@@ -0,0 +1,42 @@
+import {defineStore} from "pinia";
+
+export type AuthStateUser = {
+  id: Number
+  email: String
+}
+
+export type AuthState = {
+  accessToken: String
+  refreshToken: String
+  user: AuthStateUser
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    accessToken: null,
+    refreshToken: null,
+    user: {
+      id: null,
+      email: null
+    }
+  } as AuthState),
+  getters: {
+    isAuth(state: AuthState) {
+      return state.accessToken !== null
+    },
+    userId(state: AuthState) {
+      return state.user.id
+    },
+    userEmail(state: AuthState) {
+      return state.user.email
+    }
+  },
+  actions: {
+    set(state: AuthState) {
+      this.accessToken = state.accessToken
+      this.refreshToken = state.refreshToken
+      this.user.id = state.user.id
+      this.user.email = state.user.email
+    }
+  }
+})
