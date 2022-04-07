b29b6f873fd7ab8e5ef4cab59b55b8f915f1288f
Memory Clutter <memclutter@gmail.com>
2022-04-07 11:37:33 +0300

    Added validator for registration form

--- web/spa/src/components/RegistrationForm.vue
+++ web/spa/src/components/RegistrationForm.vue
@@ -1,28 +1,59 @@
 <script setup lang="ts">
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import authRegistration from '@/apis/endpoints/auth/registration'
+import type {FormInstance} from "element-plus";
 
+const ruleFormRef = ref<FormInstance>()
 const form = reactive({
   email: '',
-  password: ''
+  password: '',
+  repeatPassword: ''
 })
 
-function submit() {
-  const {data, isFinished} = authRegistration(form)
-  console.log(data)
+const rules = reactive({
+  email: [
+    { required: true, message: 'Field is required', trigger: 'blur' },
+    { type: 'email', message: 'Field is not correct email address', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: 'Field is required', trigger: 'blur' },
+  ],
+  repeatPassword: [
+    { required: true, message: 'Field is required', trigger: 'blur' },
+  ]
+})
+
+const submit = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      const {data, isFinished} = authRegistration(form)
+      console.log(data)
+    } else {
+      console.log('error', fields)
+    }
+  })
 }
 </script>
 
 <template>
-  <el-form :model="form" label-width="120px">
-    <el-form-item label="Email">
+  <el-form
+    ref="ruleFormRef"
+    label-width="120px"
+    :model="form"
+    :rules="rules"
+  >
+    <el-form-item label="Email" prop="email">
       <el-input type="email" v-model="form.email"/>
     </el-form-item>
-    <el-form-item label="Password">
+    <el-form-item label="Password" prop="password">
       <el-input type="password" v-model="form.password"/>
     </el-form-item>
+    <el-form-item label="Repeat password" prop="repeatPassword">
+      <el-input type="password" v-model="form.repeatPassword"/>
+    </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submit">Registration</el-button>
+      <el-button type="primary" @click="submit(ruleFormRef)">Registration</el-button>
     </el-form-item>
   </el-form>
 </template>
