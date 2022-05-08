1857f389e0367444612d976949b570217e41c4ee
Memory Clutter <memclutter@gmail.com>
2022-05-08 13:14:08 +0300

    Upgrade database migrations

--- migrations/20220326115144_init.down.sql
+++ migrations/20220326115144_init.down.sql
@@ -1,11 +1,34 @@
-ALTER TABLE confirmations
-    DROP CONSTRAINT fk_confirmations_user_id;
-ALTER TABLE tasks
-    DROP CONSTRAINT fk_tasks_user_id;
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Foreign keys drop
+-- ---------------------------------------------------------------------------------------------------------------------
+ALTER TABLE task_participants DROP CONSTRAINT fk_task_participants_user_id;
+ALTER TABLE task_participants DROP CONSTRAINT fk_task_participants_task_id;
+ALTER TABLE tasks DROP CONSTRAINT fk_tasks_custom_status_id;
+ALTER TABLE tasks DROP CONSTRAINT fk_tasks_project_id;
+ALTER TABLE project_statuses DROP CONSTRAINT fk_project_statuses_project_id;
+ALTER TABLE access DROP CONSTRAINT fk_access_project_id;
+ALTER TABLE access DROP CONSTRAINT fk_access_group_id;
+ALTER TABLE access DROP CONSTRAINT fk_access_user_id;
+ALTER TABLE projects DROP CONSTRAINT fk_projects_group_id;
+ALTER TABLE confirmations DROP CONSTRAINT fk_confirmations_user_id;
 
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Table drops
+-- ---------------------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS task_participants;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS project_statuses;
+DROP TABLE IF EXISTS access;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS confirmations;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS tasks;
 
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Custom types drops
+-- ---------------------------------------------------------------------------------------------------------------------
+DROP TYPE project_status;
+DROP TYPE group_status;
+DROP TYPE access_role;
 DROP TYPE user_status;
 DROP TYPE task_status;
--- migrations/20220326115144_init.up.sql
+++ migrations/20220326115144_init.up.sql
@@ -1,25 +1,23 @@
-CREATE TYPE task_status AS ENUM ('todo', 'inProgress', 'closed');
-CREATE TYPE user_status AS ENUM ('pending', 'active', 'block');
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Create custom types
+-- ---------------------------------------------------------------------------------------------------------------------
+CREATE TYPE task_status AS ENUM ('active', 'deleted');
+CREATE TYPE user_status AS ENUM ('pending', 'active', 'deleted');
+CREATE TYPE access_role AS ENUM ('member', 'admin');
+CREATE TYPE group_status AS ENUM ('active', 'archive', 'deleted');
+CREATE TYPE project_status AS ENUM ('active', 'archive', 'deleted');
 
-CREATE TABLE IF NOT EXISTS tasks
-(
-    id           BIGSERIAL,
-    user_id      BIGINT NOT NULL,
-    title        TEXT,
-    description  TEXT,
-    date_created TIMESTAMP   DEFAULT NOW(),
-    status       task_status DEFAULT 'todo',
-    PRIMARY KEY (id)
-);
-
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Create tables
+-- ---------------------------------------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users
 (
-    id         BIGSERIAL,
-    first_name VARCHAR(500) NOT NULL,
-    last_name  VARCHAR(500) NOT NULL,
-    email      VARCHAR(500) NOT NULL,
-    pw_hash    VARCHAR(500) NOT NULL,
-    status     user_status DEFAULT 'pending',
+    id            BIGSERIAL,
+    first_name    VARCHAR(500) NOT NULL,
+    last_name     VARCHAR(500) NOT NULL,
+    email         VARCHAR(500) NOT NULL,
+    password_hash VARCHAR(500) NOT NULL,
+    status        user_status DEFAULT 'pending',
     PRIMARY KEY (id)
 );
 
@@ -33,12 +31,99 @@ CREATE TABLE IF NOT EXISTS confirmations
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS groups
+(
+    id     BIGSERIAL,
+    name   VARCHAR(255) NOT NULL,
+    status group_status DEFAULT 'active',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS projects
+(
+    id       BIGSERIAL,
+    group_id BIGINT       NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    status   project_status DEFAULT 'active',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS access
+(
+    id         BIGSERIAL,
+    user_id    BIGINT NOT NULL,
+    group_id   BIGINT,
+    project_id BIGINT,
+    role       access_role DEFAULT 'member',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS project_statuses
+(
+    id         BIGSERIAL,
+    project_id BIGINT       NOT NULL,
+    name       VARCHAR(128) NOT NULL,
+    sort_order SMALLINT DEFAULT 0,
+    is_final   BOOLEAN  DEFAULT FALSE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id               BIGSERIAL,
+    project_id       BIGINT NOT NULL,
+    title            TEXT,
+    description      TEXT,
+    date_created     TIMESTAMP   DEFAULT NOW(),
+    status           task_status DEFAULT 'active',
+    custom_status_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS task_participants
+(
+    id      BIGSERIAL,
+    task_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Create indexes
+-- ---------------------------------------------------------------------------------------------------------------------
 CREATE UNIQUE INDEX uix_users_email ON users (email);
 CREATE UNIQUE INDEX uix_confirmations_token ON confirmations (token);
 
-ALTER TABLE tasks
-    ADD CONSTRAINT fk_tasks_user_id FOREIGN KEY (user_id) REFERENCES users (id)
-        ON UPDATE CASCADE ON DELETE RESTRICT;
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Create foreign keys
+-- ---------------------------------------------------------------------------------------------------------------------
 ALTER TABLE confirmations
     ADD CONSTRAINT fk_confirmations_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE projects
+    ADD CONSTRAINT fk_projects_group_id FOREIGN KEY (group_id) REFERENCES groups (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE access
+    ADD CONSTRAINT fk_access_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE access
+    ADD CONSTRAINT fk_access_group_id FOREIGN KEY (group_id) REFERENCES groups (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE access
+    ADD CONSTRAINT fk_access_project_id FOREIGN KEY (project_id) REFERENCES projects (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE project_statuses
+    ADD CONSTRAINT fk_project_statuses_project_id FOREIGN KEY (project_id) REFERENCES projects (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE tasks
+    ADD CONSTRAINT fk_tasks_project_id FOREIGN KEY (project_id) REFERENCES projects (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE tasks
+    ADD CONSTRAINT fk_tasks_custom_status_id FOREIGN KEY (custom_status_id) REFERENCES project_statuses (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE task_participants
+    ADD CONSTRAINT fk_task_participants_task_id FOREIGN KEY (task_id) REFERENCES tasks (id)
+        ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE task_participants
+    ADD CONSTRAINT fk_task_participants_user_id FOREIGN KEY (user_id) REFERENCES users (id)
         ON UPDATE CASCADE ON DELETE CASCADE;
