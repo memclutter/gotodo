4955d1562b9ed2d34246a3a93db35d16ed01b9c1
Memory Clutter <memclutter@gmail.com>
2022-05-27 23:58:53 +0300

    Implement status create

--- internal/api/endpoints/statuses.go
+++ internal/api/endpoints/statuses.go
@@ -1,6 +1,11 @@
 package endpoints
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/memclutter/gotodo/internal/api/schemas"
+	"github.com/memclutter/gotodo/internal/models"
+	"net/http"
+)
 
 // StatusesCreate godoc
 //
@@ -15,8 +20,20 @@ import "github.com/labstack/echo/v4"
 // @Failure				400				{object}		schemas.Error					true	"Validation error"
 // @Failure				500				{object}		schemas.Error					true	"Server error"
 // @Security			ApiHeaderAuth
-func StatusesCreate(c echo.Context) error {
-	return nil
+func StatusesCreate(c echo.Context) (err error) {
+	req := schemas.StatusesRequest{}
+	if err = c.Bind(&req); err != nil {
+		return err
+	} else if err = c.Validate(&req); err != nil {
+		return err
+	}
+	project := models.Status{
+		ProjectID: req.ProjectID,
+		Name:      req.Name,
+		SortOrder: req.SortOrder,
+		IsFinal:   req.IsFinal,
+	}
+	return c.JSON(http.StatusCreated, project)
 }
 
 func StatusesUpdate(c echo.Context) error {
--- internal/api/schemas/statuses.go
+++ internal/api/schemas/statuses.go
@@ -4,4 +4,5 @@ type StatusesRequest struct {
 	ProjectID int64  `json:"projectId" validate:"required"`
 	Name      string `json:"name" validate:"required"`
 	IsFinal   bool   `json:"isFinal"`
+	SortOrder int    `json:"sort_order"`
 }
