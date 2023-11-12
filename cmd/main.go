d28d139406a0eae08a35de1c1a1f8e0c111fdc1e
Memory Clutter <memclutter@gmail.com>
2023-11-12 09:01:11 +0300

    Addedd action

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"
+	"log"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	(&cli.App{}).Run(os.Args)
+	app := &cli.App{
+		Name:  "gotodo",
+		Usage: "Golang app",
+		Action: func(*cli.Context) error {
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
