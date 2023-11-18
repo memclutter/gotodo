fc358223838430366c6c66a88c6120fcec4c5f1d
Memory Clutter <memclutter@gmail.com>
2023-11-18 22:23:54 +0300

    Added init database migration file

--- migrations/db/000001_init.up.sql
+++ migrations/db/000001_init.up.sql
@@ -1,3 +1,9 @@
-CREATE TABLE tasks (
-    id BIGSERIAL,
-)
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id           BIGSERIAL,
+    title        TEXT,
+    body         TEXT,
+    is_done      BOOLEAN   DEFAULT FALSE,
+    date_created TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
